2ffda9c522a33915eb93ea3ef74c0f8a2577f8eb
Segundo <segundogaldames@gmail.com>
2023-05-16 06:49:21 -0400

    Editar roles

--- controllers/rolesController.php
+++ controllers/rolesController.php
@@ -19,7 +19,6 @@ class rolesController extends Controller
 
     public function create()
     {
-
         $this->_view->titulo = 'Roles';
         $this->_view->process = 'roles/store';
         $this->_view->send = HASH_KEY;
@@ -66,5 +65,48 @@ class rolesController extends Controller
         $this->_view->titulo = 'Roles';
         $this->_view->rol = $rol;
         $this->_view->render('show');
+    }
+
+    public function edit($id = null)
+    {
+        $rol = $this->_rol->getRolId($this->filtrarInt($id));
+
+        if (!$rol) {
+            $this->redireccionar('error/error');
+        }
+
+        $this->_view->titulo = 'Roles';
+        $this->_view->rol = $rol;
+        $this->_view->process = "roles/update/{$id}";
+        $this->_view->send = HASH_KEY;
+        $this->_view->render('edit');
+    }
+
+    public function update($id = null)
+    {
+        if ($this->getSql('send') == HASH_KEY) {
+
+            if (!$this->getTexto('nombre')) {
+                Session::set('msg_error','Debe ingresar el nombre del rol');
+                $this->redireccionar('roles/edit/' . $id);
+            }
+
+            $rol = $this->_rol->editRol(
+                $this->filtrarInt($id),
+                ucfirst($this->getTexto('nombre'))
+            );
+
+            if (!$rol) {
+                Session::set('msg_error','El rol no se ha modificado... intente nuevamente');
+                $this->redireccionar('roles');
+            }
+
+            Session::set('msg_success','El rol se ha modificado correctamente');
+            $this->redireccionar('roles/show/' . $id);
+
+        }else {
+        throw new Exception("Error Processing Request", 1);
+
+        }
     }
 }
